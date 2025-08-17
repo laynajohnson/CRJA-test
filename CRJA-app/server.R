b74d3346e4725e349b6835ebeb3c861c86f227bd
laynajohnson <117413709+laynajohnson@users.noreply.github.com>
2025-08-17 17:16:02 -0500

    website

--- CRJA-app/server.R
+++ CRJA-app/server.R
@@ -1,6 +1,7 @@
 
 
 library(shiny)
+library(shinyjs)
 
 library(tidyverse)
 library(ggplot2)
@@ -8,24 +9,41 @@ library(htmltools)
 
 # Define server logic for selections and downloads
 function(input, output, session) {
-
-  output$value <- renderText({input$select})
   
- 
+  library(shinyjs)
+  
+  report_ready <- reactiveVal(FALSE)
+  
+  shinyjs::disable("report") # disable download until ready
+  
+  # Generate the report
+  observeEvent(input$generate, {
+    Sys.sleep(1)  # pretend processing time
+    report_ready(TRUE)
+    shinyjs::enable("report")
+  })
+  
   output$report <- downloadHandler(
     filename = "report.pdf",
     content = function(file) {
+      if (!report_ready()) return(NULL) # prevent download if not ready
+      
       tempReport <- file.path(tempdir(), "report.Rmd")
       file.copy("report.Rmd", tempReport, overwrite = TRUE)
       
       rmarkdown::render(
         input = tempReport,
         output_file = file,
-        params = list(n = input$select_county),
+        params = list(
+          county = input$select_county,
+          ethrace = input$select_ethrace,
+          gender = input$select_gender
+        ),
         envir = new.env(parent = globalenv())
       )
     }
   )
-  
-
 }
+
+
+# to render app/update it: rsconnect::deployApp()
--- CRJA-app/ui.R
+++ CRJA-app/ui.R
@@ -16,18 +16,24 @@ counties <- c(
   "Stanislaus", "Sutter", "Tehama", "Trinity", "Tulare", "Tuolumne", "Ventura", "Yolo", "Yuba"
 )
 
+ethnicityrace <- c("Asian", "Black", "Hispanic", "Native American", "White")
 
+gender <- c("Male", "Female", "Other", "Prefer not to respond")
+
+# yearcharged <- c("")
+# 
+# charge <- c("")
 
 
 # Define UI for application
 fluidPage(
-  
+  useShinyjs(),
 
     # Application title
   tags$style(HTML("
     .title-panel {
-      background-color: #873418;
-      color: white;
+      background-color: white;
+      color: black;
       padding: 7px;
       font-family: Lato;
       font-weight: 600;
@@ -42,18 +48,32 @@ fluidPage(
     sidebarLayout(
         sidebarPanel(
           width = 4,
-          h3(em("Some title")),
+          h3(em("Create Your Report")),
           p("Some text about selecting things, user can search to select"),
           selectInput(
             "select_county",
             "Select County:",
             choices = counties,
             selectize = TRUE
-          )
+          ),
+          selectInput(
+            "select_ethrace",
+            "Select Race/Ethnicity:",
+            choices = ethnicityrace,
+            selectize = TRUE
+          ),
+          selectInput(
+            "select_gender",
+            "Select Gender:",
+            choices = gender,
+            selectize = TRUE
+          ),
+          actionButton("generate", "Generate Report", class = "btn-primary")
         ),
-    # Show results and download button
+        
+    # Show results and download button (after generating report with action button)
         mainPanel(
-          downloadButton("report", "Download Report")
+          downloadButton("report", "Download Report", class = "btn-success", disabled = "disabled")
         )
     )
 )
