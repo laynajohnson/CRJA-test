a34706e8b69345d1f1434fd7c2c680ae92f2d459
Alayna Johnson <117413709+laynajohnson@users.noreply.github.com>
2025-08-28 13:27:03 -0500

    edits

--- CRJA-app/server.R
+++ CRJA-app/server.R
@@ -7,7 +7,7 @@ library(tidyverse)
 library(ggplot2)
 library(htmltools)
 
-# Define server logic for selections and downloads
+# server logic for selections and downloads
 function(input, output, session) {
   
   library(shinyjs)
@@ -16,9 +16,30 @@ function(input, output, session) {
   
   shinyjs::disable("report") # disable download until ready
   
-  # Generate the report
+  # Generate the report and save data to csv
   observeEvent(input$generate, {
     Sys.sleep(1)  # pretend processing time
+    
+    df <- data.frame(
+      county      = input$select_county,
+      race        = input$select_race,
+      yearoffense = input$select_yearoffense,
+      penalcode   = input$select_penalcode,
+      enhancements = input$select_enhancements,
+      ethnicity   = input$select_ethnicity,
+      natorigin   = input$select_natorigin,
+      timestamp   = Sys.time(), # would it be helpful to know the website's traffic using this?
+      stringsAsFactors = FALSE
+    )
+    
+    file_path <- "selections.csv"
+    if (!file.exists(file_path)) {
+      write.csv(df, file_path, row.names = FALSE)
+    } else {
+      write.table(df, file_path, sep = ",", col.names = FALSE,
+                  row.names = FALSE, append = TRUE)
+    }
+    
     report_ready(TRUE)
     shinyjs::enable("report")
   })
@@ -26,7 +47,7 @@ function(input, output, session) {
   output$report <- downloadHandler(
     filename = "report.pdf",
     content = function(file) {
-      if (!report_ready()) return(NULL) # prevent download if not ready
+      if (!report_ready()) return(NULL) # prevent download if user has not clicked "generate report"
       
       tempReport <- file.path(tempdir(), "report.Rmd")
       file.copy("report.Rmd", tempReport, overwrite = TRUE)
@@ -36,8 +57,13 @@ function(input, output, session) {
         output_file = file,
         params = list(
           county = input$select_county,
-          ethrace = input$select_ethrace,
-          gender = input$select_gender
+          race = input$select_race,
+          yearoffense = input$select_yearoffense,
+          penalcode = input$select_penalcode,
+          enhancements = input$select_enhancements,
+          ethnicity = input$select_ethnicity,
+          natorigin = input$select_natorigin
+          
         ),
         envir = new.env(parent = globalenv())
       )
--- CRJA-app/ui.R
+++ CRJA-app/ui.R
@@ -5,7 +5,11 @@ library(shinythemes)
 library(shinyWidgets)
 library(shinyjs)
 
-counties <- c(
+library(tidyverse)
+
+test_data <- read_csv("randomly_selected_data.xlsx")
+
+counties <- c("Select Option",
   "Alameda", "Alpine", "Amador", "Butte", "Calaveras", "Colusa", "Contra Costa", "Del Norte",
   "El Dorado", "Fresno", "Glenn", "Humboldt", "Imperial", "Inyo", "Kern", "Kings", "Lake", "Lassen",
   "Los Angeles", "Madera", "Marin", "Mariposa", "Mendocino", "Merced", "Modoc", "Mono", "Monterey",
@@ -15,20 +19,25 @@ counties <- c(
   "Stanislaus", "Sutter", "Tehama", "Trinity", "Tulare", "Tuolumne", "Ventura", "Yolo", "Yuba"
 )
 
-ethnicityrace <- c("Asian", "Black", "Hispanic", "Native American", "White")
+race <- unique(test_data$Race) # can recode vars Mexican and Cuban options to Hispanic
 
-gender <- c("Male", "Female", "Other", "Prefer not to respond")
+ethnicity <- unique(test_data$Ethnicity) # A bit more confusing... what to do here?
 
-# yearcharged <- c("")
+natorigin <- unique(test_data$`Place of Birth`) # can recode all of the states to be United States
+
+yearoffense <- 1970:2025
 # 
-# charge <- c("")
+penalcode <- unique(test_data$Offense)
+
+enhancements <- unique(test_data$Off_Enh1, test_data$Off_Enh2) %>%
+  na.omit()
 
 
 # Define UI for application
 fluidPage(
   useShinyjs(),
 
-    # Application title
+    # Specifying style for the website title and levels of text and headings
   tags$style(HTML("
     .title-panel {
       background-color: white;
@@ -36,6 +45,12 @@ fluidPage(
       padding: 7px;
       font-family: Lato;
       font-weight: 600;
+    }
+  ")),
+  
+  tags$style(HTML("
+  h1, h2, h3, h4, h5, h6, p {
+    font-family: Lato;
     }
   ")),
   
@@ -47,8 +62,8 @@ fluidPage(
     sidebarLayout(
         sidebarPanel(
           width = 4,
-          h3(em("Create Your Report")),
-          p("Some text about selecting things, user can search to select"),
+          h3(em("Create Court Report")),
+          p("Select from the options in the menus below to fit the specific case. User can type to search and select options."),
           selectInput(
             "select_county",
             "Select County:",
@@ -56,22 +71,53 @@ fluidPage(
             selectize = TRUE
           ),
           selectInput(
-            "select_ethrace",
-            "Select Race/Ethnicity:",
-            choices = ethnicityrace,
+            "select_yearoffense",
+            "Select Year of Conviction:",
+            choices = yearoffense,
             selectize = TRUE
           ),
           selectInput(
-            "select_gender",
-            "Select Gender:",
-            choices = gender,
+            "select_penalcode",
+            "Select Penal Code Sections:",
+            choices = penalcode,
+            selectize = TRUE,
+            multiple = TRUE
+          ),
+          selectInput(
+            "select_enhancements",
+            "Select Enhancement(s) (Optional):",
+            choices = enhancements,
+            selectize = TRUE,
+            multiple = TRUE
+          ),
+          selectInput(
+            "select_race",
+            "Select Race of Interest:",
+            choices = race,
             selectize = TRUE
           ),
+          selectInput(
+            "select_ethnicity",
+            "Select Ethnicity of Interest:",
+            choices = ethnicity,
+            selectize = TRUE
+          ),
+          selectInput(
+            "select_natorigin",
+            "Select Nation of Origin:",
+            choices = natorigin,
+            selectize = TRUE
+          ),
+          p('After selection, click the "Generate Report" button below to see statistics and generate a court-ready document for download.'),
           actionButton("generate", "Generate Report", class = "btn-primary")
         ),
         
     # Show results and download button (after generating report with action button)
         mainPanel(
+          h3("Using the Dashboard"),
+          p("Disclaimer for intentions of the app and what the goals are..."),
+          br(),
+          h4("Download the generated report below:"),
           downloadButton("report", "Download Report", class = "btn-success", disabled = "disabled")
         )
     )
